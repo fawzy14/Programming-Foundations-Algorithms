9133a0a03a8d824edc57c89eef90708b31e32932
fawzy14 <71606754+fawzy14@users.noreply.github.com>
2021-11-01 18:33:53 +0200

    queue

---
+++ queue.py
@@ -0,0 +1,12 @@
+names = []    #queues 
+
+names.append('Mahmoud')   # in queues last in, first out
+names.append('Omer')      # in queues first in, last out 
+names.append('Fawzy')
+names.append('Abeer')
+
+print(names)  
+
+remove_name = names.pop()  #abeer will out 
+print(remove_name)
+print(names)
