badd52f0e4c0b78ffa5f57ff1918b32867b988dc
fawzy14 <71606754+fawzy14@users.noreply.github.com>
2021-11-04 05:59:37 +0200

    unordered list search

---
+++ unordered-list-search.py
@@ -0,0 +1,17 @@
+# searching for an item in an unordered list
+# sometimes called a Linear search
+
+# declare a list of values to operate on
+items = [6, 20, 8, 19, 56, 23, 87, 41, 49, 53]
+
+def find_item(item, itemlist):
+
+    for i in range(0, len(items)):
+        
+        if item == itemlist[i]:
+            return i 
+    
+    return None
+
+print(find_item(56, items))
+print(find_item(500, items))
